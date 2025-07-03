5e7d30995fd9f15988fd74017b443c834a39d4f1
Graham Klingler <grahamklingler@gmail.com>
2025-07-02 21:48:08 -0700

    Add introductory text box

--- vite-app/src/pages/Home/Home.tsx
+++ vite-app/src/pages/Home/Home.tsx
@@ -1,11 +1,25 @@
+/* Home.tsx */
 
 import HomeNavBar from "../../components/HomeNavBar/HomeNavBar"
+import styles from "./Home.module.css"
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
 
 function Home() {
     return (
         <>
             <HomeNavBar/>
-            <div></div>
+            <div className={styles.centerWindow}>
+                <div className={styles.windowContent}>
+                    <div className={styles.logoSection}>
+                        <MonitorHeartIcon className={styles.bigLogo} />
+                    </div>
+                    <div className={styles.textSection}>
+                        <h1 className={styles.heading}>Welcome to MediData</h1>
+                        <p className={styles.subtext}>Your health, organized and accessible.</p>
+                        <button className={styles.getStartedBtn}>Getting Started</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
